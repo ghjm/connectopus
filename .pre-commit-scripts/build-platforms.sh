1bbca5731dc38f83fb24916ee5fbdb0a01df55d7
Graham Mainwaring <graham@mhn.org>
2022-06-02 21:48:58 -0400

    Actually call cleanup

--- .pre-commit-scripts/build-platforms.sh
+++ .pre-commit-scripts/build-platforms.sh
@@ -5,6 +5,7 @@ cleanup() {
   rm -f testbin
   exit $ARG
 }
+trap cleanup EXIT
 GOOS=linux   go build -o testbin cmd/connectopus.go
 GOOS=windows go build -o testbin cmd/connectopus.go
 GOOS=darwin  go build -o testbin cmd/connectopus.go
