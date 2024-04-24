2b0a3b60d40e3b4729e0fc5400039058fc2583cc
Graham Mainwaring <graham@mhn.org>
2024-04-24 17:55:28 -0400

    Update eslint to flat config format

---
+++ ui/eslint.config.js
@@ -0,0 +1,10 @@
+let eslint = require('@eslint/js');
+let tselint = require('typescript-eslint');
+
+module.exports = [
+    eslint.configs.recommended,
+    ...tselint.configs.recommended,
+    { 
+        files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/*.ts', '**/*.tsx'],
+    },
+];
